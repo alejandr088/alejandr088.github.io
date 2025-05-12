f9c053b8a22f449f59bc6896c81f653b79705c22
alejandr088 <alejandrivello@gmail.com>
2025-05-12 17:46:37 -0300

    added new tb funct

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,15 @@ menuToggle.addEventListener('click', () => {
   menuToggle.setAttribute('aria-expanded', !isExpanded);
   menu.classList.toggle('open');
 });
+
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    navbar.classList.remove('bg-black/40', 'backdrop-blur-md');
+    navbar.classList.add('bg-black', 'shadow-lg');
+  } else {
+    navbar.classList.remove('bg-black', 'shadow-lg');
+    navbar.classList.add('bg-black/40', 'backdrop-blur-md');
+  }
+});
